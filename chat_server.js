54e75386a93fd526a349c53d99595173c42492bd
djmorosini <djmorosini@outlook.com>
2018-07-26 20:27:55 -0400

    setting up chat server using objects

--- chat_server.js
+++ chat_server.js
@@ -1,64 +1,135 @@
 const http = require('http');
 const mime = require('mime-types');
 const Assistant = require('./lib/assistant');
+const House = require('./lib/house')
 const port = process.env.PORT || 5000;
-let messages = [];
+let house = new House()
+
+// const Message = require('./lib/message')
+// let getNumber = 1
 
 http.createServer(handleRequest).listen(port)
 console.log("Listening on port: " + port);
 
 function handleRequest(request, response) {
-    console.log('request.url = ' + request.url)
-    let assistant = new Assistant(request, response)
-    let path = assistant.path
+  console.log('request.url = ' + request.url)
+  let assistant = new Assistant(request, response)
+  let path = assistant.path
 
-    function sendMessages() {
-        let data = JSON.stringify(messages);
-        let type = mime.lookup('json')
-        assistant.finishResponse(type, data)
-    }
+  let roomId = path.slice(1).split('/')[1]
 
-    try {
-        if (request.url === "/") {
-            assistant.sendFile('index.html')
+  let extraRequest
+  let identifier
+  let value
 
-        } else if (request.url === "/chat") {
+  if (request.url.includes('?')) {
+    extraRequest = request.url.split('?')[1]
+    [identifier, value] = extraRequest.split("=")
+  }
+  
+  function sendResponse(messages) {
+    let data = JSON.stringify(messages);
+    let type = mime.lookup('json')
+    assistant.finishResponse(type, data)
+  }
 
-            if (request.method === "POST") {
-                console.log('Parsing the POST')
-                assistant.parsePostParams((params) => {
-                    let message = {
-                        author: 'Anonymous',
-                        body: params.body,
-                        when: new Date().toISOString()
-                    }
-                    messages.push(message);
-                    sendMessages()
-                })
-            } else { // GET
-                if (messages.length > 0) {
-                    // let mostRecentMessageAt = messages[messages.length - 1].when
-                    // console.log(mostRecentMessageAt)
-                    // fetch('/chat?since=' + mostRecentMessageAt, {
-                    //     method: 'GET'
-                    // })
-                    //     .then(response => response.json())
-                    //     .then(messages => {
-                    //         sendMessages()
-                    //     })
+  try {
+    if (path === "/") {
+      assistant.sendFile('./public/index.html')
 
+    } else if (path === "/chat") {
 
-                    sendMessages()
+      if (request.method === "POST") {
+        console.log('Parsing the POST')
+        assistant.parsePostParams((params) => {
+          let message = {
+            author: 'Anonymous',
+            body: params.body || 'no body',
+            when: new Date().toISOString()
+          }
+          if (!roomId) {
+            roomId = 'general'
+          }
+          house.sendMessageToRoom(roomId, message);
+          let room = house.roomWithId(roomId)
+          let messages = room.messagesSince(0)
 
-                } else {
-                    console.log('No messages!')
-                }
-            }
-        } else {
-            let fileName = request.url.slice(1)
-            assistant.sendFile(fileName)
+          sendResponse(messages)
+        })
+      } else { // GET
+
+        // test messages
+        // if (getNumber === 1) {
+        //   dogMessage = new Message({ author: 'Dallas', body: 'woof' })
+        //   generalMessage = new Message({ author: 'Dylan', body: 'general message' })
+        //   house.sendMessageToRoom('general', generalMessage)
+        //   house.sendMessageToRoom('dogs', dogMessage)
+        //   // test messages
+        //   getNumber++
+        // }
+        let allRooms = house.allRoomIds()
+        let roomMessages = []
+
+        if (extraRequest && identifier === 'since') {
+          for (let roomId of allRooms) {
+            theRoom = house.roomWithId(roomId)
+            messagesSince = theRoom.messagesSince(value)
+            roomMessages.push(messagesSince)
+          }
+          sendResponse(roomMessages)
         }
-    } catch (error) {
-        assistant.sendError(404, "Error: " + error.toString())
+        // else if (extraRequest && identifier==='body') {
+
+        // } else if (extraRequest && identifier==='author') {
+
+        // } 
+        else {
+          for (let roomId of allRooms) {
+            theRoom = house.roomWithId(roomId)
+            roomMessages.push(theRoom.messages)
+          }
+          sendResponse(roomMessages)
+        }
+      }
+    } else if (path === `/chat/${roomId}`) {
+
+      if (request.method === "POST") {
+        console.log('Parsing the POST')
+        assistant.parsePostParams((params) => {
+          let message = {
+            author: 'Anonymous',
+            body: params.body || 'no body',
+            when: new Date().toISOString()
+          }
+          house.sendMessageToRoom(roomId, message);
+          let room = house.roomWithId(roomId)
+          let messages = room.messagesSince(0)
+          sendResponse(messages)
+        })
+      } else {
+
+        let room = house.roomWithId(roomId)
+
+        if (extraRequest && identifier === 'since') {
+          let messages = room.messagesSince(value)
+          sendResponse(messages)
+        }
+        // else if (extraRequest && identifier==='body') {
+
+        // } else if (extraRequest && identifier==='author') {
+
+        // } 
+        else {
+          let messages = room.messagesSince(0)
+          sendResponse(messages)
+        }
+
+      }
+    } else {
+      let fileName = request.url.slice(1)
+      assistant.sendFile(fileName)
     }
+  } catch (error) {
+    assistant.sendError(404, "Error: " + error.toString())
+  }
 }
